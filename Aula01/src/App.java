1a21e7b5503c221d325b704a92795d3965579979
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-04-22 00:50:07 -0300

    subindo arquivos

---
+++ Aula01/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+        int a, b, soma;
+
+        a = 10;
+        b = 20;
+        soma = a + b;
+        System.out.println("A soma é : " + soma);
+    }
+}
