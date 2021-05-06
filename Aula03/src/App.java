5a071eb2afe54a9e390494bb9e9107f7c109c00a
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-05-06 00:00:56 -0300

    subindo arquivos aula de java

--- Aula03/src/App.java
+++ Aula03/src/App.java
@@ -1,5 +1,43 @@
 public class App {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        int nota = 65;
+
+        // teste condicional ou desvio de fluxo
+        if(nota >= 70)
+        {
+            System.out.println("Aprovado");
+        }
+        else if(nota >= 40)
+        {
+            System.out.println("Recuperação");
+        }
+        else
+        {
+            System.out.println("Reprovado");
+        }
+
+        // Operação ternário
+        String res;
+
+        res = (nota >= 70 ? "Aprovado": "Reprovado");
+        System.out.println("Resultado: " + res);
+
+        // switch - case
+        int pos = 1;
+        switch(pos)
+        {
+            case 1:
+                System.out.println("Primeiro Lugar");
+                break;
+            case 2:
+                System.out.println("Segundo Lugar");
+                break;
+            case 3:
+                System.out.println("Terceiro Lugar");
+                break;
+            default:
+                System.out.println("N~~ao subiu no podio");
+                break;
+        }
     }
 }
