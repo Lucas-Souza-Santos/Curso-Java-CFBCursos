d3da795b2a59c61b6fd284f04044f452929250ba
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-05-06 00:13:31 -0300

    aula nova

--- aula04/src/App.java
+++ aula04/src/App.java
@@ -2,6 +2,19 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
 
+        int n1 = 0;
+        int n2 = 0;
+        int res = 0;
+
+        System.out.print("Digite um número: ");
+        n1 = entrada.nextInt();
+        System.out.print("Digite um numero: ");
+        n2 = entrada.nextInt();
+
+        res = n1 + n2;
+
+        System.out.println("O resultado é: " + res);
     }
 }
