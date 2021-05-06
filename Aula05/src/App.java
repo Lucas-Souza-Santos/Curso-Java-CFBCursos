4a4e971f6751e9adceb5179b58dbc5170702e740
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-05-06 00:22:04 -0300

    subindo arquivos

--- Aula05/src/App.java
+++ Aula05/src/App.java
@@ -1,5 +1,24 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        
+        // loop for
+        for(int i = 0; i < 4; i++)
+        {
+            System.out.println("i = " + i);
+        }
+        // loop while
+        int cont = 0;
+        while(cont < 5)
+        {
+            System.out.println("cont = " + cont);
+            cont++;
+        }
+        int num = 0;
+        do
+        {
+            System.out.println(num);
+            num++;
+        }
+        while(num < 10);
     }
 }
