633ab72e4cadd10247e7f1b1343a43b1e779ca8d
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-05-06 16:02:18 -0300

    subindo arquvivos

--- Aula06/src/App.java
+++ Aula06/src/App.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args){
         int[] num = new int[5];
@@ -5,10 +7,16 @@ public class App {
         num[0] = 1;
         num[4] = 10;
         num[1] = 5;
+        num[2] = 9;
+        num[3] = 5;
+
+        Arrays.sort(num);
 
         for (int i : num) {
             System.out.println("num = " + i);
         }
+
+        
 
     }
 }
